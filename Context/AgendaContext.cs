0bcfc9bccb725a0050e5f71f89d73ae3cfb57757
Marcos Vinicius Conceição Oliveira <vinioliveira2008y@gmail.com>
2024-10-09 16:57:23 -0300

    feat: adicionar alguns atributos a minha class Agenda

--- Context/AgendaContext.cs
+++ Context/AgendaContext.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Microsoft.Net.Http.Headers;
+using ModuloAPI.Entities;
 
 namespace ModuloAPI.Context
 {
-    public class AgendaContext
+    public class AgendaContext : DbContext   
     {
-        
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> contatos( get; set; )
     }
 }
