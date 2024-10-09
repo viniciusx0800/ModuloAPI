bf6008b5be6daae3d28dcf9f681a8a44bccb395a
Marcos Vinicius Conceição Oliveira <vinioliveira2008y@gmail.com>
2024-10-09 17:16:05 -0300

    build: dependências externas

--- obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd804fee2338f75a58dc075e6095b5a6b55a4ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea04531231250c1d89949132b29fbbb476a0275f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
