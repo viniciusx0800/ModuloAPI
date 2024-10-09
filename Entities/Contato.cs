1108db411e2d7c5966ec019afa7fd990a1023dfe
Marcos Vinicius Conceição Oliveira <vinioliveira2008y@gmail.com>
2024-10-09 16:35:05 -0300

    feat: adicionar alguns atributos a minha class contato

--- Entities/Contato.cs
+++ Entities/Contato.cs
@@ -7,6 +7,9 @@ namespace ModuloAPI.Entities
 {
     public class Contato
     {
-        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
     }
 }
