0a8f85d1ab225277a735d7a2b54d53973857c4f6
Marcos Vinicius Conceição Oliveira <vinioliveira2008y@gmail.com>
2024-10-10 16:29:19 -0300

    feat: Adicionar as endopointes para id, e update

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,12 +1,61 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModuloAPI.Context;
+using ModuloAPI.Entities;
 
 namespace ModuloAPI.Controllers
 {
-    public class ContatoController
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
     {
-        
+        private readonly AgendaContext _context;
+        private object contato;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato) 
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return NotFound();
+
+            return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var  contatoBanco = _context.Contatos.Find(id);
+            
+            if (contatoBanco == null)
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
     }
 }
